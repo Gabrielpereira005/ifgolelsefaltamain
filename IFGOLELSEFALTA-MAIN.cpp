ecdcbdb725851e05eda231b054169362fcb8bc8e
Gabrielpereira005 <gabrielmoricazarin@gmail.com>
2025-11-10 20:25:30 -0300

    Update IFGOLELSEFALTA-MAIN.cpp
    modificaçao na aula de 10/11
    em relaçao a chamada dos times e funçoes

--- IFGOLELSEFALTA-MAIN.cpp
+++ IFGOLELSEFALTA-MAIN.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <string>  // <-- Adicionado
+#include <vector>  // <-- Adicionado
+
 using namespace std;
 
-int main()
+void ExibirMenu()
 {
-	int modo;
-	
 	cout << "Seja bem-vindo ao IF GOL ELSE FALTA" << endl;
 	cout << " " << endl;
 	cout << "Selecione a modalidade que deseja simular seu torneio:" << endl;
@@ -15,134 +16,179 @@ int main()
 	cout << "3 - PREMIERE LEAGUE" << endl;
 	cout << "4 - CHAMPIONS LEAGUE" << endl;
 	cout << "5 - FACA VOCE MESMO" << endl;
+}
+
+void SelecBrasileirao()
+{
+	cout << "Voce selecionou o modo BRASILEIRAO" << endl;
 	
+	// 1. Criar o array (compatível com C++98)
+	string times_array[] = {
+		"Palmeiras", "Flamengo", "Cruzeiro", "Mirassol", "Bahia",
+		"Botafogo", "Fluminense", "Sao Paulo", "Vasco", "Corinthians",
+		"Gremio", "Ceara", "Atletico Mineiro", "RB Bragantino",
+		"Internacional", "Santos", "Vitoria", "Fortaleza",
+		"Juventude", "Sport"
+	};
+	int num_times = sizeof(times_array) / sizeof(times_array[0]);
+	
+	// 2. Criar o vetor a partir do array
+	vector<string> times(times_array, times_array + num_times);
+	
+	cout << "Times participantes:" << endl;
+	// 3. Listar os times usando o loop
+	for(size_t i = 0; i < times.size(); ++i) {
+		cout << (i + 1) << " - " << times[i] << endl;
+	}
+	
+	// 4. Pedir a seleção do time
+	int timeEscolhido;
+	cout << "\nEscolha o seu time (digite o numero): ";
+	cin >> timeEscolhido;
+
+	if(timeEscolhido >= 1 && timeEscolhido <= (int)times.size()) {
+		cout << "Voce escolheu: " << times[timeEscolhido - 1] << "!" << endl;
+	} else {
+		cout << "Selecao invalida." << endl;
+	}
+}
+
+void SelecLaliga()
+{
+	cout << "Voce selecionou o modo LA LIGA" << endl;
+	
+	string times_array[] = {
+		"Real Madrid", "Barcelona", "Vilarreal", "Atletico de Madrid",
+		"Real Betis", "Espanyol", "Getafe", "Alaves", "Elche",
+		"Rayo Vallencano", "Athletic Club", "Celta", "Sevilla",
+		"Real Sociedad", "Osasuna", "Levante", "Mallorca", "Valencia",
+		"Real Oviedo", "Girona"
+	};
+	int num_times = sizeof(times_array) / sizeof(times_array[0]);
+	vector<string> times(times_array, times_array + num_times);
+
+	cout << "Times participantes:" << endl;
+	for(size_t i = 0; i < times.size(); ++i) {
+		cout << (i + 1) << " - " << times[i] << endl;
+	}
+
+	int timeEscolhido;
+	cout << "\nEscolha o seu time (digite o numero): ";
+	cin >> timeEscolhido;
+
+	if(timeEscolhido >= 1 && timeEscolhido <= (int)times.size()) {
+		cout << "Voce escolheu: " << times[timeEscolhido - 1] << "!" << endl;
+	} else {
+		cout << "Selecao invalida." << endl;
+	}
+}
+
+void SelecPremierLeague()
+{
+	cout << "Voce selecionou o modo PREMIERE LEAGUE" << endl;
+	
+	string times_array[] = {
+		"Arsenal", "Manchester City", "Liverpool", "Sunderland",
+		"Bournemouth", "Tottenham", "Chelsea", "Manchester United",
+		"Crystal Palace", "Brighton", "Aston Villa", "Brentford",
+		"Newcastle", "Everton", "Fullham", "Leeds", "Burnley",
+		"West Ham", "Nottingham Forest", "Wolves"
+	};
+	int num_times = sizeof(times_array) / sizeof(times_array[0]);
+	vector<string> times(times_array, times_array + num_times);
+
+	cout << "Times participantes:" << endl;
+	for(size_t i = 0; i < times.size(); ++i) {
+		cout << (i + 1) << " - " << times[i] << endl;
+	}
+
+	int timeEscolhido;
+	cout << "\nEscolha o seu time (digite o numero): ";
+	cin >> timeEscolhido;
+
+	if(timeEscolhido >= 1 && timeEscolhido <= (int)times.size()) {
+		cout << "Voce escolheu: " << times[timeEscolhido - 1] << "!" << endl;
+	} else {
+		cout << "Selecao invalida." << endl;
+	}
+}
+
+void SelecChampionsLeague()
+{
+	cout << "Voce selecionou o modo CHAMPIONS LEAGUE" << endl;
+
+	string times_array[] = {
+		"PSG", "Bayern", "Inter", "Arsenal", "Real Madrid", "Borussia",
+		"Manchester City", "Newcastle", "Barcelona", "Liverpool",
+		"Chelsea", "Sporting", "Qarabag", "Galatasaray", "Tottenham",
+		"PSV", "Atalanta", "Marseille", "Atletico de Madrid",
+		"Club Brugge", "Athletic Club", "Frankfurt", "Napoli",
+		"USG", "Juventus", "Glimt", "AS Monaco", "Slavia Praha",
+		"Pafos", "Leverkusen", "Villareal", "Kobenhavn",
+		"Olympiacos", "Kairat", "Benfica", "Ajax"
+	};
+	int num_times = sizeof(times_array) / sizeof(times_array[0]);
+	vector<string> times(times_array, times_array + num_times);
+	
+	cout << "Times participantes:" << endl;
+	for(size_t i = 0; i < times.size(); ++i) {
+		cout << (i + 1) << " - " << times[i] << endl;
+	}
+
+	int timeEscolhido;
+	cout << "\nEscolha o seu time (digite o numero): ";
+	cin >> timeEscolhido;
+
+	if(timeEscolhido >= 1 && timeEscolhido <= (int)times.size()) {
+		cout << "Voce escolheu: " << times[timeEscolhido - 1] << "!" << endl;
+	} else {
+		cout << "Selecao invalida." << endl;
+	}
+}
+
+void SelecFacaVoceMesmo ()
+{
+	cout << "Voce selecionou o modo FACA VOCE MESMO" << endl;
+	// Aqui você pode adicionar a lógica para o usuário digitar
+	// os próprios nomes dos times.
+}
+
+int main()
+{
+	int modo;
+
+	ExibirMenu();
 	cin >> modo;
-	
+
 	if(modo == 1)
 	{
-		cout << "Voce selecionou o modo BRASILEIRAO" << endl;
-		cout << "Times participantes:" << endl;
-		cout << "Palmeiras" << endl;
-		cout << "Flamengo" << endl;
-		cout << "Cruzeiro" << endl;
-		cout << "Mirassol" << endl;
-		cout << "Bahia" << endl;
-		cout << "Botafogo" << endl;
-		cout << "Fluminense" << endl;
-		cout << "Sao Paulo" << endl;
-		cout << "Vasco" << endl;
-		cout << "Corinthians" << endl;
-		cout << "Gremio" << endl;
-		cout << "Ceara" << endl;
-		cout << "Atletico Mineiro" << endl;
-		cout << "RB Bragantino" << endl;
-		cout << "Internacional" << endl;
-		cout << "Santos" << endl;
-		cout << "Vitoria" << endl;
-		cout << "Fortaleza" << endl;
-		cout << "Juventude" << endl;
-		cout << "Sport" << endl;
+		SelecBrasileirao();
+		// cin >> modo;  <-- Removido (não precisa ler de novo)
 	}
-	
 	else if(modo == 2)
 	{
-		cout << "Voce selecionou o modo LA LIGA" << endl;
-		cout << "Times participantes:" << endl;
-		cout << "Real Madrid" << endl;
-		cout << "Barcelona" << endl;
-		cout << "Vilarreal" << endl;
-		cout << "Atletico de Madrid" << endl;
-		cout << "Real Betis" << endl;
-		cout << "Espanyol" << endl;
-		cout << "Getafe" << endl;
-		cout << "Alaves" << endl;
-		cout << "Elche" << endl;
-		cout << "Rayo Vallencano" << endl;
-		cout << "Athletic Club" << endl;
-		cout << "Celta" << endl;
-		cout << "Sevilla" << endl;
-		cout << "Real Sociedad" << endl;
-		cout << "Osasuna" << endl;
-		cout << "Levante" << endl;
-		cout << "Mallorca" << endl;
-		cout << "Valencia" << endl;
-		cout << "Real Oviedo" << endl;
-		cout << "Girona" << endl;
+		SelecLaliga();
+		// cin >> modo;  <-- Removido
 	}
-	
 	else if(modo == 3)
 	{
-		cout << "Voce selecionou o modo PREMIERE LEAGUE" << endl;
-		cout << "Times participantes:" << endl;
-		cout << "Arsenal" << endl;
-		cout << "Manchester City" << endl;
-		cout << "Liverpool" << endl;
-		cout << "Sunderland" << endl;
-		cout << "Bournemouth" << endl;
-		cout << "Tottenham" << endl;
-		cout << "Chelsea" << endl;
-		cout << "Manchester United" << endl;
-		cout << "Crystal Palace" << endl;
-		cout << "Brighton" << endl;
-		cout << "Aston Villa" << endl;
-		cout << "Brentford" << endl;
-		cout << "Newcastle" << endl;
-		cout << "Everton" << endl;
-		cout << "Fullham" << endl;
-		cout << "Leeds" << endl;
-		cout << "Burnley" << endl;
-		cout << "West Ham" << endl;
-		cout << "Nottingham Forest" << endl;
-		cout << "Wolves" << endl;
+		SelecPremierLeague();
+		// cin >> modo;  <-- Removido
 	}
-	
 	else if(modo == 4)
 	{
-		cout << "Voce selecionou o modo CHAMPIONS LEAGUE" << endl;
-		cout << "Times participantes:" << endl;
-		cout << "PSG" << endl;
-		cout << "Bayern" << endl;
-		cout << "Inter" << endl;
-		cout << "Arsenal" << endl;
-		cout << "Real Madrid" << endl;
-		cout << "Borussia" << endl;
-		cout << "Manchester City" << endl;
-		cout << "Newcastle" << endl;
-		cout << "Barcelona" << endl;
-		cout << "Liverpool" << endl;
-		cout << "Chelsea" << endl;
-		cout << "Sporting" << endl;
-		cout << "Qarabag" << endl;
-		cout << "Galatasaray" << endl;
-		cout << "Tottenham" << endl;
-		cout << "PSV" << endl;
-		cout << "Atalanta" << endl;
-		cout << "Marseille" << endl;
-		cout << "Atletico de Madrid" << endl;
-		cout << "Club Brugge" << endl;
-		cout << "Athletic Club" << endl;
-		cout << "Frankfurt" << endl;
-		cout << "Napoli" << endl;
-		cout << "USG" << endl;
-		cout << "Juventus" << endl;
-		cout << "Glimt" << endl;
-		cout << "AS Monaco" << endl;
-		cout << "Slavia Praha" << endl;
-		cout << "Pafos" << endl;
-		cout << "Leverkusen" << endl;
-		cout << "Villareal" << endl;
-		cout << "Kobenhavn" << endl;
-		cout << "Olympiacos" << endl;
-		cout << "Kairat" << endl;
-		cout << "Benfica" << endl;
-		cout << "Ajax" << endl;
-		
+		SelecChampionsLeague();
+		// cin >> modo;  <-- Removido
 	}
-	
 	else if(modo == 5)
 	{
-		cout << "Voce selecionou o modo FACA VOCE MESMO" << endl;
+		SelecFacaVoceMesmo();
+		// cin >> modo;  <-- Removido
 	}
-	
+	else
+	{
+		cout << "Opcao invalida!" << endl;
+	}
+
 	return 0;
 }
